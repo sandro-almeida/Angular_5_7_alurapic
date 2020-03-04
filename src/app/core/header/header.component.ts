c28096486c5e33f9a53eff0f354070c105842196
Sandro Almeida <sandro001@gmail.com>
2020-03-04 17:27:15 -0300

    Header displays logged user - using BehaviorSubject

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
+import { UserService } from './../user/user.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user/user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ap-header',
@@ -7,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  user$: Observable<User>; //$ at the end of the variable to indicate it is an Observable
+  user: User;
+
+  constructor(userService: UserService) { 
+    this.user$ = userService.getUser(); //it returns an Observable
+    this.user$.subscribe(usuario => this.user = usuario);
+  }
 
   ngOnInit() {
   }
--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import * as jwt_decode from 'jwt-decode';   //this is the import command of the jwt decoder
 import { TokenService } from './../token/token.service';
@@ -9,7 +9,7 @@ import { User } from './user';
 })
 export class UserService {
 
-  private userSubject = new Subject<User>();
+  private userSubject = new BehaviorSubject<User>(null);
 
   constructor(private tokenService: TokenService) { 
     this.tokenService.hasToken() &&
