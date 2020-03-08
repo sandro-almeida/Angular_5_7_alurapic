c710080a402e8268f04d2cb06186f687584fa9b3
Sandro Almeida <sandro001@gmail.com>
2020-03-08 10:32:59 -0300

    Form validation

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+        email: ['',
+            [
+                Validators.required,
+                Validators.email
+            ]
+        ],
+        fullName: ['',
+            [
+                Validators.required,
+                Validators.minLength(2),
+                Validators.maxLength(40)
+            ]
+        ],
+        userName: ['',
+            [
+                Validators.required,
+                Validators.pattern(/^[a-z0-9_\-]+$/),
+                Validators.minLength(2),
+                Validators.maxLength(30)
+            ]
+        ],
+        password: ['',
+            [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.maxLength(14)
+            ]
+        ]
+    });
   }
 
 }
