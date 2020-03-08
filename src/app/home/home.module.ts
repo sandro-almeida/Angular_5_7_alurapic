638397c5cfe466ce2b1f7033f9c2e5b2c1004d5f
Sandro Almeida <sandro001@gmail.com>
2020-03-08 09:53:35 -0300

    Add signUp component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormsModule,
     VmessageModule,
     RouterModule
   ],
-  declarations: [SigninComponent]
+  declarations: [
+    SigninComponent, 
+    SignupComponent
+  ]
 })
 export class HomeModule { }
