ee12763ae34c58f69790bf88cdd9919a7ead5b69
Sandro Almeida <sandro001@gmail.com>
2020-03-08 19:02:31 -0300

    Using interceptors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
   imports: [
@@ -9,6 +11,13 @@ import { HeaderComponent } from './header/header.component';
     RouterModule
   ],
   declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent],
+  providers: [
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: RequestInterceptor,
+        multi: true
+    }
+]
 })
 export class CoreModule { }
