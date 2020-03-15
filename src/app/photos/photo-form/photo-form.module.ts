279ab5ad21b207f46d4f5a132e20e9dc9f5670c6
Sandro Almeida <sandro001@gmail.com>
2020-03-15 12:20:46 -0300

    New form for uploading photos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,16 @@
+import { VmessageModule } from './../../shared/components/vmessage/vmessage.module';
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [ CommonModule ]
+    imports: [ 
+        CommonModule,
+        ReactiveFormsModule,
+        VmessageModule,
+        FormsModule
+     ]
 })
 export class PhotoFormModule { }
