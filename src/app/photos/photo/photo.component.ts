4a08742e7a93ae4f3c20dac18141668cad6ebb80
Sandro Almeida <sandro001@gmail.com>
2020-03-15 20:54:08 -0300

    Inbound property using set

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,12 +1,27 @@
 import { Component, Input } from '@angular/core';
 
+const CLOUD = 'http://localhost:3000/imgs/';
+
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
+
+    private _url: string = '';
     
     @Input() description='';
     
-    @Input() url='';
+    //this inbound property can be set directly using obj.url = "123"
+    @Input() set url(url: string) {
+        if (!url.startsWith('data')) { //the url of all photos that come from http://localhost:3000/ start with 'data:image'
+            this._url = CLOUD + url;
+        } else {
+            this._url = url;
+        }
+    }
+
+    get url() {
+        return this._url;
+    }
 }
