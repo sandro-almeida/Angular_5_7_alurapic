a763a6377c5347ab5a9f59d0d336e0308686088b
Sandro Almeida <sandro001@gmail.com>
2020-03-17 18:14:15 -0300

    Parameterized route for photo details

---
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './photo-details.component.html',
+  styleUrls: ['./photo-details.component.css']
+})
+export class PhotoDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
