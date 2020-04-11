3986c25861e2a61f5ec07367c2c4e1bda28ee790
Sandro Almeida <sandro001@gmail.com>
2020-04-11 16:50:04 -0300

    Implement alert component

---
+++ src/app/shared/components/alert/alert.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input } from "@angular/core";
+import { Alert, AlertType } from './alert';
+import { AlertService } from './alert.service';
+
+@Component({
+    selector: 'ap-alert',
+    templateUrl: './alert.component.html'
+})
+export class AlertComponent {
+
+    @Input() timeout = 3000; //Inbound property initialized with 3 seconds
+    alerts: Alert[] = [];
+
+    constructor(private alertService: AlertService) {
+
+        this.alertService
+            .getAlert()
+            .subscribe(alert => {
+                if(!alert) {
+                    this.alerts = [];   //clean the alerts if the received alert is null
+                    return;
+                }
+
+                this.alerts.push(alert);
+                setTimeout(() => this.removeAlert(alert), this.timeout);
+            });
+    }
+
+    removeAlert(alertToRemove: Alert) {
+        this.alerts = this.alerts.filter(alert => alert != alertToRemove);  //removes the alert passed as a parameter
+    }
+
+    //This method returns a bootstrap class
+    getAlertClass(alert:  Alert) {
+
+        if(!alert) return '';
+
+        switch (alert.alertType) {
+
+            case AlertType.DANGER:
+                return 'alert alert-danger';
+            case AlertType.INFO:
+                return 'alert alert-info';
+            case AlertType.SUCCESS:
+                return 'alert alert-success';
+            case AlertType.WARNING:
+                return 'alert alert-warning';
+
+        }
+    }
+}
