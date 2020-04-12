5e99701bdcab3d47c7f4cda2fd07b297a038f7ac
Sandro Almeida <sandro001@gmail.com>
2020-04-12 11:55:49 -0300

    Attribute directive to verify if user is logged and hide DOM element if it is not logged

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -7,6 +7,7 @@ import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoModule } from './../photo/photo.module';
 import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
 import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.directive';
+import { ShowIfLoggedModule } from './../../shared/directives/show-if-logged/show-if-logged.module';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.dir
     PhotoModule,
     RouterModule,
     ReactiveFormsModule,
-    VmessageModule
+    VmessageModule,
+    ShowIfLoggedModule
   ],
   declarations: [
     PhotoDetailsComponent, 
