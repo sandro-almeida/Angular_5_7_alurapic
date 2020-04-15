1f3a43ac96d9e532de7044476e16b508b22ed5b0
Sandro Almeida <sandro001@gmail.com>
2020-04-15 18:46:13 -0300

    Create interceptor http

--- src/app/shared/components/loading/loading.module.ts
+++ src/app/shared/components/loading/loading.module.ts
@@ -1,3 +1,5 @@
+import { LoadingInterceptor } from './loading.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from "./loading.component";
@@ -5,6 +7,11 @@ import { LoadingComponent } from "./loading.component";
 @NgModule({
     declarations: [ LoadingComponent ],
     exports: [ LoadingComponent ],
-    imports: [ CommonModule]
+    imports: [ CommonModule],
+    providers: [{
+        provide: HTTP_INTERCEPTORS,
+        useClass: LoadingInterceptor,
+        multi: true
+    }]
 })
 export class LoadingModule { }
