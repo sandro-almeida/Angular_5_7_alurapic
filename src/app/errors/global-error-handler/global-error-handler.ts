1ef21702c303cfe9b79a0b0ae3791c267a1dd7e2
Sandro Almeida <sandro001@gmail.com>
2020-04-16 18:19:27 -0300

    Install and use stacktrace.js for handling errors

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,9 +1,18 @@
 import { ErrorHandler } from "@angular/core";
+import * as StackTrace from "stacktrace-js";
 
 export class GlobalErrorHandler implements ErrorHandler { 
 
     handleError(error: any): void {
-        console.log('Passei pelo Error Handler !');
-        throw error;
+        console.log('passei pelo handler')
+        StackTrace
+            .fromError(error)
+            .then(stackFrames => {
+                console.log('StackFrames: ', stackFrames);
+                const stackAsString = stackFrames
+                .map(sf => sf.toString())
+                .join('\n');
+                console.log('StackFrames as string: ', stackAsString);
+            })
     }
 }
