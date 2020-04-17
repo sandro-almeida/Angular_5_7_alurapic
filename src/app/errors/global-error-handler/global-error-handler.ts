eb918963f2b7894e19edef80c46173ad90d84952
Sandro Almeida <sandro001@gmail.com>
2020-04-17 18:22:01 -0300

    Add log server and post error log to server

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -2,6 +2,7 @@ import { ErrorHandler, Injectable, Injector } from "@angular/core";
 import * as StackTrace from "stacktrace-js";
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { UserService } from 'src/app/core/user/user.service';
+import { ServerLogService } from './server-log-service';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler { 
@@ -13,6 +14,7 @@ export class GlobalErrorHandler implements ErrorHandler {
 
         const location = this.injector.get(LocationStrategy); //dependency injection on demand
         const userService = this.injector.get(UserService);
+        const serverLogService = this.injector.get(ServerLogService);
 
         const url = location instanceof PathLocationStrategy //get the current route when error occurred
             ? location.path() : '';
@@ -31,7 +33,18 @@ export class GlobalErrorHandler implements ErrorHandler {
                 console.log('Error message: ', message);
                 console.log('StackFrames as string: ', stackAsString);
                 console.log('O que será enviado para o servidor: ');
-                console.log({ message, url, userName: userService.getUserName(), stack: stackAsString});
+                serverLogService.log({ 
+                    message, 
+                    url, 
+                    userName: userService.getUserName(), 
+                    stack: stackAsString}
+                ).subscribe(
+                    () => console.log('Error logged successfully on server !'),
+                    err => {
+                        console.log(err);
+                        console.log('Fail to send error log to server !');
+                    }
+                )
             });
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://myProductionUrl.com'
+  apiUrl: 'http://myProductionUrl.com',
+  serverLog: 'http://localhost:7000'
 };
