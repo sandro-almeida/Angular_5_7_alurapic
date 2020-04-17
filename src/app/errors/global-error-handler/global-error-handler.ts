848780528923c45145d5e8005e458685668b3bcc
Sandro Almeida <sandro001@gmail.com>
2020-04-17 17:03:11 -0300

    Capture and display front end error information to be logged on the server

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,10 +1,25 @@
-import { ErrorHandler } from "@angular/core";
+import { ErrorHandler, Injectable, Injector } from "@angular/core";
 import * as StackTrace from "stacktrace-js";
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { UserService } from 'src/app/core/user/user.service';
 
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler { 
 
+    constructor(private injector: Injector) {}
+
     handleError(error: any): void {
-        console.log('passei pelo handler')
+        console.log('passei pelo handler');
+
+        const location = this.injector.get(LocationStrategy); //dependency injection on demand
+        const userService = this.injector.get(UserService);
+
+        const url = location instanceof PathLocationStrategy //get the current route when error occurred
+            ? location.path() : '';
+        
+        const message = error.message
+            ? error.message : error.toString(); //message may not exist
+
         StackTrace
             .fromError(error)
             .then(stackFrames => {
@@ -12,7 +27,11 @@ export class GlobalErrorHandler implements ErrorHandler {
                 const stackAsString = stackFrames
                 .map(sf => sf.toString())
                 .join('\n');
+
+                console.log('Error message: ', message);
                 console.log('StackFrames as string: ', stackAsString);
-            })
+                console.log('O que será enviado para o servidor: ');
+                console.log({ message, url, userName: userService.getUserName(), stack: stackAsString});
+            });
     }
 }
